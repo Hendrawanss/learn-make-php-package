ec9326a2d26ba8c5500838e71a0cff3b3e345193
Hendrawan Sulistyanto <hendrawanss123@gmail.com>
2025-04-14 15:36:40 +0700

    Add autoload and some class

---
+++ src/Human.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Awan\TestPackage;
+
+class Human
+{
+    public function head(): string
+    {
+        return 'This is head!';
+    }
+
+    public function body(): string
+    {
+        return 'This is body!';
+    }
+    
+    public function hand(): string
+    {
+        return 'This is hand!';
+    }
+
+    public function leg(): string
+    {
+        return 'This is leg!';
+    }
+}
